e1157f9978c3dafebb557dbfca34708db9038fad
UsuariooRoot <burnedprogrammer@gmail.com>
2025-03-30 18:27:40 -0500

    feat(features/incidents): show content in owned section

--- src/app/features/incidents/pages/owned/owned.component.ts
+++ src/app/features/incidents/pages/owned/owned.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '@core/authentication/auth.service';
+import { User } from '@core/authentication/interface';
+import { FiltersComponent } from '@features/incidents/components/filters/filters.component';
+import {
+  IncidentTableComponent,
+  TicketRow,
+} from '@features/incidents/components/incident-table/incident-table.component';
+import { IncidentService } from '@features/incidents/services/incident.service';
+import { mapIncidentTicketToRowTicket } from '@features/incidents/utils/mappers';
+import { IncidentTicketFilter } from '@shared/in-mem/api';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-owned',
-  imports: [],
+  imports: [IncidentTableComponent, FiltersComponent],
   templateUrl: './owned.component.html',
-  styleUrl: './owned.component.scss'
+  styleUrl: './owned.component.scss',
 })
 export class OwnedComponent {
+  private readonly incidentService = inject(IncidentService);
+  private readonly authService = inject(AuthService);
+  private user$: Observable<User> = this.authService.user();
 
+  idItTeam = 0;
+  ticketRows: TicketRow[] = [];
+
+  ngOnInit(): void {
+    this.user$.subscribe({
+      next: user => {
+        this.idItTeam = this.getIdItTeam(user);
+        this.getTicketRows({ employee_owner: user.id });
+      },
+    });
+  }
+
+  getTicketRows(filter?: IncidentTicketFilter) {
+    this.incidentService.getIncidentTickets(filter).subscribe({
+      next: data => {
+        console.log(data);
+        this.ticketRows = data.map(mapIncidentTicketToRowTicket);
+      },
+      error: err => console.error('Error al obtener incidentes:', err),
+    });
+  }
+
+  getIdItTeam(user: User): number {
+    return user?.id_it_team ?? 0;
+  }
 }
