f3426cbd0c3f3ba36b02f10f10d6182ae63d0457
UsuariooRoot <burnedprogrammer@gmail.com>
2025-05-06 08:19:51 -0500

    refactor(features/incidents): add reactive forms to the create ticket section

--- src/app/features/incidents/pages/create/create.component.ts
+++ src/app/features/incidents/pages/create/create.component.ts
@@ -1,8 +1,8 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IncidentService } from '@features/incidents/services/incident.service';
 import { ToastrService } from 'ngx-toastr';
-import { SelectComponent } from "../../../../shared/components/select/select.component";
+import { SelectComponent } from "@shared/components/select/select.component";
 
 @Component({
   selector: 'app-create',
@@ -10,22 +10,23 @@ import { SelectComponent } from "../../../../shared/components/select/select.com
   styleUrl: './create.component.scss',
   imports: [
     SelectComponent,
-    FormsModule,
+    ReactiveFormsModule,
 ],
 })
 export class CreateComponent implements OnInit {
   private readonly incidentService = inject(IncidentService);
   private readonly toast = inject(ToastrService);
+  private readonly formBuilder = inject(FormBuilder)
 
-  newIncident = {
-    incident: '',
-    description: '',
-  };
+  ticketForm = this.formBuilder.group({
+    incident: ['', Validators.required],
+    description: ['']
+  })
 
   incidentCategory = []
 
   submit() {
-    this.toast.success(JSON.stringify(this.newIncident));
+    this.toast.success(JSON.stringify(this.ticketForm.value));
   }
 
   ngOnInit() {
