53bd8b06c56349e71daa3349a50ffab6b86b0d8c
UsuariooRoot <burnedprogrammer@gmail.com>
2025-05-16 13:41:27 -0500

    feat: enhance filters component with employee loading and error handling

--- src/app/features/incidents/components/filters/filters.component.ts
+++ src/app/features/incidents/components/filters/filters.component.ts
@@ -1,10 +1,12 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { FilterService } from '@features/incidents/services/filter.service';
 import { NgxPermissionsModule } from 'ngx-permissions';
 import { SelectComponent } from '@shared/components/select/select.component';
 import { Employee, TicketStatus } from '@features/incidents/typings';
+import { Subject, takeUntil } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 export interface GenericEmployee {
   id: number;
@@ -26,9 +28,11 @@ export interface IncidentTicketFilter {
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.scss',
 })
-export class FiltersComponent implements OnInit {
+export class FiltersComponent implements OnInit, OnChanges, OnDestroy {
   private readonly filterService = inject(FilterService);
+  private readonly toastr = inject(ToastrService);
   private readonly formBuilder = inject(FormBuilder);
+  private destroy$ = new Subject<void>();
 
   @Input() itTeamId!: number;
 
@@ -50,31 +54,50 @@ export class FiltersComponent implements OnInit {
     this.loadEmployees(this.itTeamId);
   }
 
-  private loadEmployees(itTeamId: number) {
-    if (itTeamId < 1) return;
-
-    this.filterService.getEmployeesByItTeam(itTeamId).subscribe({
-      next: data => {
-        this.IT_TEAM_EMPLOYEES = data.map(this.mapEmployeeToGenericEmployee);
-      },
-      error: err => console.error('Error al obtener empleados por equipo TI:', err),
-    });
-
-    this.filterService.getRequestersByItTeam(itTeamId).subscribe({
-      next: data => {
-        this.REQUESTER_EMPLOYEES = data.map(this.mapEmployeeToGenericEmployee);
-      },
-      error: err => console.error('Error al obtener solicitantes por equipo de TI:', err),
-    });
+  ngOnChanges(changes: SimpleChanges): void {
+    // If the itTeamId changes, we reload the employees
+    if (changes['itTeamId'] && !changes['itTeamId'].firstChange && this.itTeamId > 0) {
+      this.loadEmployees(this.itTeamId);
+    }
   }
 
-  private loadTicketStatuses() {
-    this.filterService.getIncidentTicketStatuses().subscribe({
-      next: statuses => {
-        this.TICKET_STATUSES = statuses;
-      },
-      error: err => console.error('Error al obtener los estados de los ticket de incidentes:', err),
-    });
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+
+  private loadEmployees(itTeamId: number): void {
+    if (itTeamId < 1) return;
+
+    this.filterService.getEmployeesByItTeam(itTeamId)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: data => {
+          this.IT_TEAM_EMPLOYEES = data.map(this.mapEmployeeToGenericEmployee);
+        },
+        error: err => this.toastr.error('Error al obtener empleados por equipo TI:', err),
+      });
+
+    this.filterService.getRequestersByItTeam(itTeamId)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: data => {
+          this.REQUESTER_EMPLOYEES = data.map(this.mapEmployeeToGenericEmployee);
+        },
+        error: err => this.toastr.error('Error al obtener solicitantes por equipo de TI:', err),
+      });
+  }
+
+   private loadTicketStatuses(): void {
+    this.filterService.getIncidentTicketStatuses()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: statuses => {
+          this.TICKET_STATUSES = statuses;
+        },
+        error: err => this.toastr.error('Error al obtener los estados de los ticket de incidentes:', err),
+      });
   }
 
   applyFilters() {
